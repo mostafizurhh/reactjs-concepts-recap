2eb5cc0df771f9ee8b3b1904b43abdd16625b452
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-09-20 21:38:09 +0100

    Components inside the folder structure and connect them

--- src/App.js
+++ src/App.js
@@ -1,39 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Country from './components/Countries/Country';
+import Header from './components/Header/Header';
+
+
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
+      {/* <LoadCountries></LoadCountries> */}
+      <Header></Header>
+      <Country></Country>
     </div>
   );
 }
 
-function LoadCountries() {
-  const [countries, setCountries] = useState([])
-  useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-      .then(res => res.json())
-      .then(data => setCountries(data))
-  }, [])
+// function LoadCountries() {
+//   const [countries, setCountries] = useState([])
+//   useEffect(() => {
+//     fetch('https://restcountries.com/v3.1/all')
+//       .then(res => res.json())
+//       .then(data => setCountries(data))
+//   }, [])
 
-  return (
-    <div className='country'>
-      <h3>All Countries of the World</h3>
-      <h4>Total countries: {countries.length}</h4>
+//   return (
+//     <div className='country'>
+//       <h3>All Countries of the World</h3>
+//       <h4>Total countries: {countries.length}</h4>
 
-      {countries.map(country => <Countries name={country.name.common}></Countries>)}
-    </div>
-  )
-}
+//       {countries.map(country => <Countries name={country.name.common}></Countries>)}
+//     </div>
+//   )
+// }
 
-function Countries(props) {
-  return (
-    <div className='countries'>
-      <h3>Name: {props.name}</h3>
-    </div>
-  )
-}
+// function Countries(props) {
+//   return (
+//     <div className='countries'>
+//       <h3>Name: {props.name}</h3>
+//     </div>
+//   )
+// }
 
 export default App;
---
+++ src/components/Countries/Country.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Country.css'
+
+const Country = () => {
+    return (
+        <div>
+            <h2>How Cool!!!</h2>
+        </div>
+    );
+};
+
+export default Country;
